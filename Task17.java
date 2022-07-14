3005c09a292e7bc351ed9aed8737f4528593bb82
Phanindrababu5868 <109127892+Phanindrababu5868@users.noreply.github.com>
2022-07-14 07:36:41 +0530

    program to digits sum of given number

---
+++ Task17.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Q17digitssum {
+    static int sumdigit(int n){
+        return n==0?0:n%10+sumdigit(n/10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter you digits:");
+        int n= sc.nextInt();
+        System.out.println("sum of digits are : "+sumdigit(n));
+    }
+}
