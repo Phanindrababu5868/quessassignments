ee85dc9673e34059bfcdd38cdecad51cbd7f5e9a
Phanindrababu5868 <109127892+Phanindrababu5868@users.noreply.github.com>
2022-07-21 16:08:20 +0530

    Update Task20.java

--- Task20.java
+++ Task20.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
-
-public class armstrong
-{
+public class task20
+{ 
     public static void main(String[] args) {
         int num1,num2;
         Scanner sc = new Scanner(System.in);
@@ -10,22 +9,26 @@ public class armstrong
         System.out.println("enter the upper range : ");
         num2  = sc.nextInt();
         System.out.println("Armstrong number between "+num1+" and "+num2+ " are : ");
-        for (int i = num1;i<num2;i++)
+        for (int i = num1;i<num2;++i)
         {
-            int check,rem,sum=0;
+            int check,s=0;
             check =i;
             while (check != 0)
             {
-                rem = check %10;
-                sum = sum+(rem*rem*rem);
                 check =check/10;
-
-
+                ++s;
+            }
+            int sum=0;
+            check=i;
+            while(check!=0){
+                int d=check%10;
+                sum+=Math.pow(d,s);
+                check/=10;
             }
             if(sum ==i){
                 System.out.println(i+" ");
             }
         }
     }
-
 }
+
